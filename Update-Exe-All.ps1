0b263438614d9732020fcf3ac79d0cd6d47eedbb
HAYAMA_Kaoru <iyahaya@nifty.com>
2024-04-25 03:53:59 +0900

    Execute for each files given as parameter

--- Update-Exe-All.ps1
+++ Update-Exe-All.ps1
@@ -1,30 +1,36 @@
 Set-PSDebug -Strict
 
+function Update-Exe($source){
+    if ( -not (Test-Path $source) ){
+        Write-Host "$source not found"
+        Exit 1
+    }
+
+    $name = (Split-Path $source -Leaf)
+
+    Get-Command $name -All | ForEach-Object {
+        $target = $_.Source
+        $dir = (Split-Path -Parent $target)
+        $answer = (Read-Host "Update `"${target}`" ? [y|n]")
+        if ( $answer -ieq "y" ){
+            Try{
+                Copy-Item $source -Destination $dir -PassThru -ErrorAction Stop
+            }
+            Catch{
+                $backup = (Join-Path $dir ($name + "-" + (Get-Random)))
+                Move-Item $target $backup -PassThru
+                Copy-Item $source -Destination $dir -PassThru
+            }
+        }
+    }
+}
+
 if ( $args.Length -lt 1 ){
     Write-Host "Update-Exe-All.ps1 EXECUTABLE-NAME"
     Exit 1
 }
-$source = $args[0]
 
-if ( -not (Test-Path $source) ){
-    Write-Host "$source not found"
-    Exit 1
+foreach ( $source in $args ){
+    Update-Exe $source
 }
 
-$name = (Split-Path $source -Leaf)
-
-Get-Command $name -All | ForEach-Object {
-    $target = $_.Source
-    $dir = (Split-Path -Parent $target)
-    $answer = (Read-Host "Update `"${target}`" ? [y|n]")
-    if ( $answer -ieq "y" ){
-        Try{
-            Copy-Item $source -Destination $dir -PassThru -ErrorAction Stop
-        }
-        Catch{
-            $backup = (Join-Path $dir ($name + "-" + (Get-Random)))
-            Move-Item $target $backup -PassThru
-            Copy-Item $source -Destination $dir -PassThru
-        }
-    }
-}
